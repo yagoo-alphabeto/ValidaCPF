ba6f35d69d11855f6806732f07095a7a5cf5fddb
yagoo-alphaneto <yagoosjn@gmail.com>
2025-02-15 17:03:02 -0300

    Subindo o Projeto

---
+++ fnvalidacpf.cs
@@ -0,0 +1,83 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Linq;
+
+
+namespace Validar_cpf
+{
+    public static class fnvalidacpf
+    {
+        [FunctionName("fnvalidacpf")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Iniciando a Validação de CPF.");
+
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            if (data?.cpf == null)
+            {
+                return new BadRequestObjectResult("Por favor, informe o CPF.");
+            }
+            string cpf =  data?.cpf;
+
+            if (!ValidaCPF(cpf) == false)
+            {
+                return new BadRequestObjectResult("CPF inválido.");
+            }
+            
+            var responseMessage = "CPF válido.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    
+
+    public static bool ValidaCPF(string cpf)
+        {
+            if (string.IsNullOrEmpty(cpf) || cpf.Length != 11 || cpf.All(c => c == cpf[0]))
+                return false;
+
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            string tempCpf = cpf.Substring(0, 9);
+            int soma = 0;
+
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+
+            int resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            string digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            soma = 0;
+
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            digito = digito + resto.ToString();
+
+            return cpf.EndsWith(digito);
+        }
+    }
+}
+
